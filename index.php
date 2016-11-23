be1995a9892474ccd1e06b846f0a05c3b6b895e1
PandoonQTN <quentin.doucet@outlook.fr>
2016-11-23 17:57:16 +0100

    nouveau wamp

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ define('APPLICATION_PATH', __DIR__ . "\application");
 define('APPLICATION_NAMESPACE', "Sistr");
 define('ROOT_PATH', __DIR__);
 require_once './framework/f3il.php';
-
 $app = \F3il\Application::getInstance(APPLICATION_PATH . "\\configuration.ini");
 $app->setDefaultControllerName("utilisateur");
 $app->run();
